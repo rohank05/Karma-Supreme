849f2374c7d511e5ccdf1a0f208314e18bdbffb6
rohank05 <33832145+rohank05@users.noreply.github.com>
2020-08-03 09:51:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,8 @@ const client = new Discord.Client();
 const fs = require('fs');
 const {token} = require('./botconfig.json')
 client.userMap = new Map()
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://chota:rohanksinha@kaa-db.rf6pp.gcp.mongodb.net/Menhera?retryWrites=true&w=majority',{useNewUrlParser: true})
-const db = mongoose.connection;
-db.on('open',()=>{
-    console.log("Mongo COnnected");
-})
-db.once('open', function(){
-    db.db.collection('guildSettings',function(err, collection){
-        console.log(collection.findOne())
-        
-    })
-})
+
+
 
 client.on('inviteCreate', async invite=>client.invite.set(invite.guild.id, await invite.guild.fetchInvites())); //the event handler was taking it needed to write extra so quick fix
 
